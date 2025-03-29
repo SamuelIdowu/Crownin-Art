079938ca547c4add4cf7cdbab3cf661614c1a44a
Samuel Idowu <samuelidowu689@gmail.com>
2025-03-29 07:47:21 +0100

    minor edits to db, admin and footer

--- utils/db.js
+++ utils/db.js
@@ -7,7 +7,7 @@ const connectDB = async () => {
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      serverSelectionTimeoutMS: 30000
+      // serverSelectionTimeoutMS: 30000
     });
     console.log('MongoDB connected successfully');
   } catch (err) {
