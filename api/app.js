f16bf2f0e401db51295051fceda48112f6a87847
Samuel Idowu <samuelidowu689@gmail.com>
2025-04-03 07:09:20 +0100

    Fix static file serving path in Express configuration

--- api/app.js
+++ api/app.js
@@ -28,7 +28,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "../views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("../public"));
+app.use(express.static("public"));
 app.use(morgan("dev"));
 
 // Routes
