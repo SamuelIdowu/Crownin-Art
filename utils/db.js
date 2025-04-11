57b7dfd0eea31262e0e5c99fd3a3945b4371d7bf
Samuel Idowu <samuelidowu689@gmail.com>
2025-04-11 04:56:46 +0100

    Uncomment MongoDB connection options in db.js and adjust routes in vercel.json

--- utils/db.js
+++ utils/db.js
@@ -5,9 +5,9 @@ require('dotenv').config();
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      // serverSelectionTimeoutMS: 30000
+      // useNewUrlParser: true,
+      // useUnifiedTopology: true,
+      // // serverSelectionTimeoutMS: 30000
     });
     console.log('MongoDB connected successfully');
   } catch (err) {
